2671832cc01e0ace328bfbe0f4591c3ae878c42f
Yogeshhampa <hcyogesh753@gmail.com>
2025-07-07 13:51:25 +0530

    git

--- src/main/java/com/shanInfotech/flightjdbcApp/App.java
+++ src/main/java/com/shanInfotech/flightjdbcApp/App.java
@@ -12,8 +12,8 @@ import com.shanInfotech.flightjdbcApp.entity.FlightDetails;
 public class App {
     public static void main(String[] args) throws Exception, SQLException{
         
-        FlightDetailsDao fd=new FlightDetailsDao();
-       fd.addFlight(new FlightDetails());
+       FlightDetailsDao fd=new FlightDetailsDao();
+      fd.addFlight(new FlightDetails());
   fd.getFlight();
 //        ToyDao td=new ToyDao();
 //        td.addToy(new Toy());
